a3b935ca1eb9012413d62a151005d995e5c17f01
ainasyazanaa <69749689+ainasyazanaa@users.noreply.github.com>
2020-11-13 01:44:14 +0800

    Initial commit

---
+++ users/user.service.js
@@ -0,0 +1,86 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+
+module.exports = {
+    authenticate,
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete
+};
+
+async function authenticate({ identityCard, password }) {
+    const patient = await db.Patient.scope('withHash').findOne({ where: { identityCard } });
+
+    if (!patient || !(await bcrypt.compare(password, patient.hash)))
+        throw 'IC Number or password is incorrect';
+
+    // authentication successful
+    const token = jwt.sign({ sub: patient.id }, config.secret, { expiresIn: '7d' });
+    return { ...omitHash(patient.get()), token };
+}
+
+async function getAll() {
+    return await db.Patient.findAll();
+}
+
+async function getById(id) {
+    return await getPatient(id);
+}
+
+async function create(params) {
+    // validate
+   if (await db.Patient.findOne({ where: { identityCard: params.identityCard } })) {
+        throw 'Ic Number "' + params.identityCard + '" is already registered';
+   }
+
+    // hash password
+    if (params.password) {
+        params.hash = await bcrypt.hash(params.password, 10);
+    }
+
+    // save user
+    await db.Patient.create(params);
+}
+
+async function update(id, params) {
+    const patient = await getPatient(id);
+
+    // validate
+    const IcSame = params.identityCard && patient.identityCard !== params.identityCard;
+    if (IcSame && await db.Patient.findOne({ where: { identityCard: params.identityCard } })) {
+        throw 'Ic Number "' + params.identityCard + '" is already registered';
+    }
+
+    // hash password if it was entered
+    if (params.password) {
+        params.hash = await bcrypt.hash(params.password, 10);
+    }
+
+    // copy params to user and save
+    Object.assign(patient, params);
+    await patient.save();
+
+    return omitHash(patient.get());
+}
+
+async function _delete(id) {
+    const patient = await getPatient(id);
+    await patient.destroy();
+}
+
+// helper functions
+
+async function getPatient(id) {
+    const patient = await db.Patient.findByPk(id);
+    if (!patient) throw 'Patient not found';
+    return patient;
+}
+
+function omitHash(patient) {
+    const { hash, ...userWithoutHash } = patient;
+    return userWithoutHash;
+}
---
+++ users/users.controller.js
@@ -0,0 +1,91 @@
+﻿const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+const validateRequest = require('_middleware/validate-request');
+const authorize = require('_middleware/authorize')
+const userService = require('./user.service');
+
+// routes
+router.post('/authenticate', authenticateSchema, authenticate);
+router.post('/register', registerSchema, register);
+router.get('/', authorize(), getAll);
+router.get('/current', authorize(), getCurrent);
+router.get('/:id', authorize(), getById);
+router.put('/:id', authorize(), updateSchema, update);
+router.delete('/:id', authorize(), _delete);
+
+module.exports = router;
+
+function authenticateSchema(req, res, next) {
+    const schema = Joi.object({
+		identityCard: Joi.string().required(),
+		password: Joi.string().required()
+
+        /*username: Joi.string().required(),
+        password: Joi.string().required()*/
+    });
+    validateRequest(req, next, schema);
+}
+
+function authenticate(req, res, next) {
+	userService.authenticate(req.body)
+        .then(patient => res.json(patient))
+        .catch(next);
+}
+
+function registerSchema(req, res, next) {
+    const schema = Joi.object({
+		identityCard: Joi.string().required(),
+        fullName: Joi.string().required(),
+		password: Joi.string().min(6).required(),
+		email: Joi.string().required(),
+		phoneNumber: Joi.string().required()
+
+    });
+    validateRequest(req, next, schema);
+}
+
+function register(req, res, next) {
+    userService.create(req.body)
+        .then(() => res.json({ message: 'Registration successful' }))
+        .catch(next);
+}
+
+function getAll(req, res, next) {
+    userService.getAll()
+        .then(patients => res.json(patients))
+        .catch(next);
+}
+
+function getCurrent(req, res, next) {
+    res.json(req.patient);
+}
+
+function getById(req, res, next) {
+	userService.getById(req.params.id)
+        .then(patient => res.json(patient))
+        .catch(next);
+}
+
+function updateSchema(req, res, next) {
+    const schema = Joi.object({
+		identityCard: Joi.string().empty(''),
+        fullName: Joi.string().empty(''),
+		password: Joi.string().min(6).empty(''),
+		email: Joi.string().empty(''),
+		phoneNumber: Joi.string().empty('')
+    });
+    validateRequest(req, next, schema);
+}
+
+function update(req, res, next) {
+    userService.update(req.params.id, req.body)
+        .then(patient => res.json(patient))
+        .catch(next);
+}
+
+function _delete(req, res, next) {
+    userService.delete(req.params.id)
+        .then(() => res.json({ message: 'Patient deleted successfully' }))
+        .catch(next);
+}
